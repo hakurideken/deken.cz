f5fd117ff70c9f4bc0c5786a656f9a1c0ccf59fc
VaclavDedekJunior <vaclav.dedek@klik.cz>
2026-06-12 11:02:38 +0200

    Initial commit – streamer website

---
+++ main.js
@@ -0,0 +1,13 @@
+function toggleMenu() {
+  document.getElementById('navMobile').classList.toggle('open');
+}
+
+document.querySelectorAll('a[href^="#"]').forEach(a => {
+  a.addEventListener('click', e => {
+    const target = document.querySelector(a.getAttribute('href'));
+    if (target) {
+      e.preventDefault();
+      target.scrollIntoView({ behavior: 'smooth', block: 'start' });
+    }
+  });
+});
